a57f810e1be1ba370c6fd1a94e7f23336cc43f07
Steve Kahn <steve@kahn.org.uk>
2019-03-07 11:46:55 +0000

    Planes cannot take off or land in a storm

--- lib/airport.py
+++ lib/airport.py
@@ -1,9 +1,20 @@
+from weather import Weather
+
 class Airport:
-    def __init__(self):
+    def __init__(self, weather = Weather()):
         self.hanger = []
+        self.weather = weather
 
     def landPlane(self, plane):
+        if self.weather.storm :
+            raise RuntimeError("Too stormy to land")
+
         self.hanger.append(plane)
 
     def takeOffPlane(self, plane):
+        if self.weather.storm :
+            raise RuntimeError("Too stormy to take off")
         self.hanger.pop(self.hanger.index(plane))
+
+    def updateWeather(self, weather = Weather()):
+        self.weather = weather
--- test/airport_test.py
+++ test/airport_test.py
@@ -6,6 +6,7 @@ if sys.path.count(os.getcwd) == 0 :
     sys.path.append(os.getcwd)
 
 from lib.airport import Airport
+from lib.weather import Weather
 
 class TestAirport(unittest.TestCase):
     def test_airport_lets_plane_land_when_sunny(self):
@@ -19,5 +20,18 @@ class TestAirport(unittest.TestCase):
         airport.takeOffPlane("Plane")
         self.assertEqual(airport.hanger.count("Plane"), 0)
 
+    def test_airport_does_not_let_plane_land_when_stormy(self):
+        airport = Airport(Weather(99))
+        with self.assertRaises(RuntimeError):
+            airport.landPlane("Plane")
+
+    def test_airport_does_not_let_plane_take_off_when_stormy(self):
+        airport = Airport(Weather(1))
+        airport.landPlane("Plane")
+        airport.updateWeather(Weather(99))
+        with self.assertRaises(RuntimeError):
+            airport.takeOffPlane("Plane")
+
+        
 if __name__ == '__main__':
     unittest.main()
